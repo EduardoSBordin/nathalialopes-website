7ecad9cbef7d9cbfc1267c53474029715c66c874
Eduardo Bordin <dudubordin26@gmail.com>
2023-10-29 21:23:01 -0300

    v0004

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const openMenu = document.querySelector('#openMenu');
+openMenu.innerHTML = '=';
 let openModalMenu = document.querySelector('#nav2');
 
 let countClick = 0;
@@ -10,11 +11,13 @@ openMenu.addEventListener('click', () => {
 
     if(countClick <= 1){
         //openModalMenu.style.display = 'flex';
+        openMenu.innerHTML = 'x';
         
     }else{
 
         countClick = 0;
         //openModalMenu.style.display = 'none';
+        openMenu.innerHTML = '=';
     }
     console.log(countClick);
 })
